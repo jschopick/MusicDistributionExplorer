6dfa2d8a9a66813b22abb76c7b2fc389d21830f9
jiawei <vccxx@hit.edu.cn>
2018-11-09 10:26:53 -0800

    using hashmap to accerate the map process

--- MusicMR/src/main/java/WordCount.java
+++ MusicMR/src/main/java/WordCount.java
@@ -23,7 +23,7 @@ import java.net.*;
 public class WordCount {
 	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 	    private final IntWritable one = new IntWritable(1);
-	    
+	    private HashMap<String, String> artistsRec = new HashMap<String, String>();
 	    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 	    	String line = value.toString();
 	    	String[] data = line.split(",");
@@ -32,34 +32,39 @@ public class WordCount {
 	    	}
 	    	String artistname = data[0].trim();
 	    	String location =data[1].trim();
-	    	String url_artistname = artistname.replaceAll(" ", "+");
-	    	String url = "http://itunes.apple.com/search?term="+url_artistname+"&media=music&entity=musicArtist";
-	    	//System.out.println(artistname + ","+location+","+url);
-	    	URL query = new URL(url);
-			URLConnection urlcon = query.openConnection();
-			InputStream is = urlcon.getInputStream();
-			byte[] rawdata = new byte[4096];
-			is.read(rawdata);
-			String jsonfmt = new String(rawdata);
-			//System.out.println(jsonfmt);
-			ObjectMapper mapper = new ObjectMapper();
-	        JsonNode root = mapper.readTree(jsonfmt);
-	        JsonNode resultc = root.path("resultCount");
-	        JsonNode results = root.path("results");
-	        if (resultc.asInt() > 0) {  
-		        if(results.isArray()) {
-		        	for(JsonNode node :results) {
-		        		String artist_name = node.path("artistName").asText();
-		        		String genre = node.path("primaryGenreName").asText();
-		        		if(genre == "") {
-		        			genre = "Mistery";
-		        		}
-		        		//System.out.println(artist_name + ","+genre+","+location);
-		        		context.write(new Text(artist_name + "," + genre+","+location), one);
-		        	}
+	    	if(artistsRec.containsKey(artistname)){
+	    		context.write(new Text(artistsRec.get(artistname)+","+location), one);
+	    	}
+	    	else {
+	    		String url_artistname = artistname.replaceAll(" ", "+");
+		    	String url = "http://itunes.apple.com/search?term="+url_artistname+"&media=music&entity=musicArtist";
+		    	//System.out.println(artistname + ","+location+","+url);
+		    	URL query = new URL(url);
+				URLConnection urlcon = query.openConnection();
+				InputStream is = urlcon.getInputStream();
+				byte[] rawdata = new byte[4096];
+				is.read(rawdata);
+				String jsonfmt = new String(rawdata);
+				//System.out.println(jsonfmt);
+				ObjectMapper mapper = new ObjectMapper();
+		        JsonNode root = mapper.readTree(jsonfmt);
+		        JsonNode resultc = root.path("resultCount");
+		        JsonNode results = root.path("results");
+		        if (resultc.asInt() > 0) {  
+			        if(results.isArray()) {
+			        	for(JsonNode node :results) {
+			        		String artist_name = node.path("artistName").asText();
+			        		String genre = node.path("primaryGenreName").asText();
+			        		if(genre == "") {
+			        			genre = "Mistery";
+			        		}
+			        		artistsRec.put(artistname, artist_name + "," + genre);
+			        		//System.out.println(artist_name + ","+genre+","+location);
+			        		context.write(new Text(artist_name + "," + genre+","+location), one);
+			        	}
+			        }
 		        }
-	        }
-	        
+	    	}
 	    }
 	}
         
