95b83de894cc189d4f0f17098215a6d705a828de
Justin Schopick <jscho004@ucr.edu>
2018-11-15 00:47:25 -0800

    Added sidebar

--- world-music-client/src/Mapbox.js
+++ world-music-client/src/Mapbox.js
@@ -39,13 +39,18 @@ class Mapbox extends Component {
     // Add zoom and rotation controls to the map.
     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
+    // Adds a marker at the specified location.
+    // new mapboxgl.Marker()
+    //   .setLngLat([30.5, 50.5])
+    //   .addTo(map);
+
     // Tracks latitude, longitude, and zoom as the user moves around the map.
     map.on('move', () => {
       const { lng, lat } = map.getCenter();
       this.setState({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
+        lng: lng.toFixed(1),
+        lat: lat.toFixed(1),
+        zoom: map.getZoom().toFixed(1)
       });
     });
 
@@ -64,8 +69,9 @@ class Mapbox extends Component {
 
     return (
       <div className="App">
+        <div className="sidebar">Country List</div>
         {/* Display Longitude, Latitude, and Zoom on top left */}
-        <div className="inline-block absolute top right mt12 mr12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+        <div className="inline-block absolute top right mt30 mr36 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
           <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
         </div>
         {/* Display Map on the Screen */}
